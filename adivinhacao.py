cb1db5169c7aa8feabc3fbef25d65f950eaf742a
NIKAO-CODE <nicolasdscience@gmail.com>
2023-10-12 15:27:58 -0300

    finaliza curso 1

--- adivinhacao.py
+++ adivinhacao.py
@@ -18,7 +18,7 @@ def jogar():
     if(nivel == 1):
         total_de_tentativas = 20
     elif(nivel == 2):
-        total_de_tentativas = 10
+        total_de_tentativas = 15
     else:
         total_de_tentativas = 5
 
